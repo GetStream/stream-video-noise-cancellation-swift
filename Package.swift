c5aa42b855ddf5714fd9bcf9c5d9936cfa764709
Anka <runner@Mac-1741195779378.local>
2025-03-05 17:51:37 +0000

    Bump 1.0.2

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .binaryTarget(
             name: "StreamVideoNoiseCancellation",
             url: "https://github.com/GetStream/stream-video-noise-cancellation-swift/releases/download/1.0.2/StreamVideoNoiseCancellation.zip",
-            checksum: "f1e3da77def12bf1ef80d9f6b6f7c220fe412a14bf4638975aacce917f090232"
+            checksum: "a3c034802e9c74979bd5afce27676aa1e3021dc7d1066eb10cdc24911fd337f3"
         ),
     ]
 )
